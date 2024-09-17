c229722b08bc143233630562cbfaa0dc7b1ea03e
robertlarousso <remi.heather@gmail.com>
2024-09-17 16:32:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ with st.form("my_form"):
     name = st.text_input("Your name ?")
     
     longitude = st.slider("longitude", 0, 100, 25)
-    latitude = st.slider("longitude", 0, 100, 25)
-    housing_median_age = st.slider("longitude", 0, 100, 25)
-    total_rooms = st.selectbox("longitude", range(50))
-    total_bedrooms = st.slider("longitude", 0, 100, 25)
-    population = st.slider("longitude", 0, 100, 25)
-    households = st.slider("longitude", 0, 100, 25)
-    median_income = st.slider("longitude", 0, 100, 25)
-    median_house_value = st.slider("longitude", 0, 100, 25)
+    latitude = st.slider("latitude", 0, 100, 25)
+    housing_median_age = st.slider("housing_median_age", 0, 100, 25)
+    total_rooms = st.selectbox("total_rooms", range(50))
+    total_bedrooms = st.slider("total_bedrooms", 0, 100, 25)
+    population = st.slider("population", 0, 100, 25)
+    households = st.slider("households", 0, 100, 25)
+    median_income = st.slider("median_income", 0, 100, 25)
+    median_house_value = st.slider("median_house_value", 0, 100, 25)
 
     submitted = st.form_submit_button("Submit")
 
