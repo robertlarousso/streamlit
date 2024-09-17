0122f69ab932d54b9836dfd54d3c2516a55802b5
robertlarousso <remi.heather@gmail.com>
2024-09-17 16:22:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,18 @@ import streamlit as st
 with st.form("my_form"):
     st.write("My Form")
     name = st.text_input("Your name ?")
+    
+    longitude = st.slider("longitude", 0, 100, 25)
+    "latitude" = st.slider("longitude", 0, 100, 25)
+    "housing_median_age" = st.slider("longitude", 0, 100, 25)
+    "total_rooms" = st.selectbox("longitude", range(50))
+    "total_bedrooms" = st.slider("longitude", 0, 100, 25)
+    "population": = st.slider("longitude", 0, 100, 25)
+    "households" = st.slider("longitude", 0, 100, 25)
+    "median_income": = st.slider("longitude", 0, 100, 25)
+    "median_house_value" = st.slider("longitude", 0, 100, 25)
 
     submitted = st.form_submit_button("Submit")
 
     if submitted:
-      st.write(f"Hi {name}")
+      st.write(f"Submitted !")
