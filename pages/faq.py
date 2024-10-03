4693e3c361fee0248ff0ad80027d0e5ce4f5aa06
robertlarousso <remi.heather@gmail.com>
2024-10-03 16:36:58 +0200

    Update faq.py

--- pages/faq.py
+++ pages/faq.py
@@ -3,14 +3,13 @@ import streamlit as st
 st.title("FAQ")
 
 # Initialize chat history
-if "faq" not in st.session_state:
-    st.session_state.faq = []
+faq = []
 
-st.session_state.faq.append({"role": "assistant", "content": "Première question de la FAQ ??"})
-st.session_state.faq.append({"role": "user", "content": "Réponse à la première question de la FAQ"})
+faq.append({"role": "assistant", "content": "Première question de la FAQ ??"})
+faq.append({"role": "user", "content": "Réponse à la première question de la FAQ"})
 
 # Display chat messages from history on app rerun
-for message in st.session_state.faq:
+for message in faq:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
